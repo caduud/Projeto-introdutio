cd442d7a4adc1fb0637f71fae229b746778777aa
Carlos Eduardo Gnatkoski <156940431+caduud@users.noreply.github.com>
2024-09-02 08:57:39 -0300

    Atividades referente a atividade de passagem de parâmetro

---
+++ Atividade 4/Atividades.cpp
@@ -0,0 +1,8 @@
+// 1 - Na linha 2 sera os valores originais a = 1, b = 2, c =3, na linha 6 sera a = 101, b = 102, c = 103 e na linha 12 mostrara os resultados originais pois a função não retorna nenhum valor
+// 2 - Na linha 2 exibira os valores da variáveis a = 1, b = 2, c = 3, na linha 6 e 12 exibiria o valor das variaveis e +100 a = 101, b = 102, c = 103
+// 3 - Na linha 7 os valores originais a = 0, b = 5, na linha 9 os dois sairam com o valor 5
+// 4 - Na linha 11, 13 e 14 saira os valores originais x = 2, y = 4, z = 6, Na linha 16 todas as variaveis sairão com valor 0 
+// 5 - Nas duas linhas 3 e 9 mostrara o valor 11
+// 6 - Na linha 4 saira o valor 8, Na linha 5 saira o valor 7, na linha 11 saira o valor 8 e na linha 12 saira o valor 2
+// 7 - Caso a for menor que b em um sistema usando for o i recebe o valor de x e o ponteiro de z recebe a soma de i + z ate i ser maior que y sendo o valor da soma, e como o ponteiro recebeu o valor, o valor que saiu no main será o resultado da soma
+// 8 - Sim pois como o z foi conectado ao ponteiro pz, o valor que pz receber z tambem recebe e na saida os dois terão o mesmo valor
