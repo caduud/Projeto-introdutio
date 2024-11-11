d8375792511d06537a0b7a6a3f2d35309bfe4380
Carlos Eduardo Gnatkoski <156940431+caduud@users.noreply.github.com>
2024-11-11 09:04:05 -0300

    Atividade 18

---
+++ Atvidade 18/1.cpp
@@ -0,0 +1,7 @@
+//Selection SORT - a complexidade do algoritimo no MELHOR/MÉDIO/PIOR caso sempre sera a mesma pois independente de como está a ordenação ele sempre ira fazer o mesmo numero de comparações
+
+//BUBBLE SORT - MELHOR CASO: quando a lista ja está ordenada e so precisa de uma passada para verificar que nada será trocado, no MÉDIO/PIOR CASO: nos dois ele precisara realizar várias trocas, tendo uma complexidade de O(n^2), mas no pior caso a lista estara toda desordenada
+
+//INSERTION SORT - MELHOR CASO: a lista ja estara toda ordenada fazendo uma verificação de cada elemento uma vez, No medio caso o algoritimo realiza uma busca binaria comparando cada elemento resultando em O(n^2) e no pior caso é quando a lista esta totalmente inversa tendo que realizar o numero maximo de trocas 
+
+//MERGE SORT - em todos os casos o algoritimo divide e ate achar o caso base, e muda quando precisa ordenar de novo apos ser divido
